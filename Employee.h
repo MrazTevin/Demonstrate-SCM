0e027eafc3505258ec49b3c5c6751ef3e4eadf68
Mraz Tevin <millatevin@gmail.com>
2022-02-11 13:15:08 +0300

    create class and declare variables

--- Employee.h
+++ Employee.h
@@ -2,4 +2,13 @@
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
 
+class Employee {
+    private:
+        string name,gender;
+        int basicSalary,allowance,netSalary;
+        float taxRate, taxAmount;
+
+
+
+};
 
