f957c37e17bd34df4d3d9fc5cdca5e3cd0d17270
Huss3n <85851097+Huss3n@users.noreply.github.com>
2022-02-11 15:24:01 +0300

    call constructor and create objects

--- EmployeeTest.cpp
+++ EmployeeTest.cpp
@@ -1,12 +1,17 @@
 /*Test driver class for class (TestAccount.cpp) */
 
 #include <iostream>
-#include "Account.h"
+#include "Employee.h"
 
 using namespace std;
 
 int main() {
-	
+	Employee newEmployee;
+	newEmployee.getDetails();
+	newEmployee.computeTaxRate();
+	newEmployee.computeTaxAmount();
+	newEmployee.computeNetSalary();
+	newEmployee.displayDetails();
 
-
+	return 0;
 }
